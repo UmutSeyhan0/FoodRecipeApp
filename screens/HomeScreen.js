ffbcd3c214b3a44b930ed330c94b5fc65f8c46e0
Umut Seyhan <umutseyhan115@gmail.com>
2025-03-02 21:22:45 +0300

    Home Screen 1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,52 @@
-import { View, Text } from 'react-native'
+import { View, Image, StatusBar, ScrollView, Text, TextInput } from 'react-native'
 import React from 'react'
+import tw, { style } from 'twrnc';
+//import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {BellIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={tw`flex-1 bg-white`}>
+    <StatusBar style="dark "/>
+    <ScrollView
+    showsVerticalScrollIndicator={false}
+    contentContainerStyle={{paddingBottom : 50}}
+    style={tw`space-y-6 pt-14`}
+    >
+
+    <View style={tw`mx-4 flex-row justify-between items-center mb-2`}>
+    <Image source={require('../assets/images/avatar.png')} style={{height : hp(5.3), width: hp(5.5)} } />
+    <BellIcon size={ hp(4)} color="gray"/>
+    </View>
+
+    <View style={tw`mx-4 space-y-2 mb-2`}>
+      <Text  style={[{fontSize: hp(1.7)}, tw` text-neutral-600`]}>Hello, Umut!</Text>
+      <View>
+      <Text style={[{fontSize: hp(3.8)}, tw`font-semibold text-neutral-600`]}>Make your own food,</Text>
+      </View>
+      <Text style={[{fontSize: hp(3.8)}, tw`font-semibold text-neutral-600`]}
+      >stay at <Text style={ tw`text-amber-400`}>home</Text></Text>
+    </View>
+
+    <View style={tw`mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]`}
+    >
+    <TextInput
+    placeholder='Search any recipe'
+    placeholderTextColor={'gray'}
+    style={[{fontSize: hp(1.7)}, tw`flex-1 text-base mb-1 pl-3 tracking-wider`]}
+    />
+    <View style={tw`bg-white rounded-full p-3`}>
+      <MagnifyingGlassIcon size={hp(2.5)} strokeWidth={3} color="gray" />
+    </View>
+
+
+
+    </View>
+
+    </ScrollView>
+     
     </View>
   )
 }
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import tw, { style } from 'twrnc';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
+import { useNavigation } from 'expo-router';
 
 
 export default function WelcomeScreen() {
@@ -10,18 +11,21 @@ export default function WelcomeScreen() {
   const ring1padding = useSharedValue(0);
   const ring2padding = useSharedValue(0);
 
+  const navigation = useNavigation();
+
   useEffect(() => {
     ring1padding.value = 0;
     ring2padding.value = 0;
     setTimeout(()=> ring1padding.value= withSpring(ring1padding.value+hp(1)), 100),
     setTimeout(()=> ring2padding.value= withSpring(ring2padding.value+hp(4.5)), 300)
 
+    setTimeout(()=> navigation.navigate('Home'), 2500)
   }, [])
 
 
   return (
   <View style={[tw`flex-1 justify-center items-center bg-amber-500`, {padding: hp(5.5) }] }>
-      <StatusBar hidden style="light"/>
+      <StatusBar  style="light"/>
 
     <Animated.View  style={[tw`bg-white/20 rounded-full`, {padding: ring2padding}]}>
 
